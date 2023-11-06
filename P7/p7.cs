f24e9e59f80b4f1d985f927a1ed30fbcdb764cce
HCJosue <149279025+HCJosue@users.noreply.github.com>
2023-11-05 20:49:43 -0400

    Update p7.cs

--- P7/p7.cs
+++ P7/p7.cs
@@ -9,24 +9,11 @@ namespace ConsoleApplication6
     class Program
     {
         public static float suma=0;
-        static void calculoip(Object  numero){
-            int n = (int)numero;
-            for (int i =0;i<n;i++) {
-                if (i % 2 == 0)
-                {
-                    suma += (1 / (2 * (float)i + 1));
-                }
-                else {
-                    suma -= (1 / (2 * (float)i + 1));
-                }
-            }
-            suma = suma * 4;
-        }
-        static void calculoip2(Object numero)
+        static void calculopi(Object numero)
         {
             float suma2 = 0;
             int n = (int)numero;
-            for (int i = 10000; i < n; i++)
+            for (int i = 10000*(n-1); i < 10000*(n); i++)
             {
                 if (i % 2 == 0)
                 {
@@ -39,50 +26,16 @@ namespace ConsoleApplication6
             }
             suma = suma+suma2 * 4;
         }
-        static void calculoip3(Object numero)
-        {
-            float suma2 = 0;
-            int n = (int)numero;
-            for (int i = 20000; i < n; i++)
-            {
-                if (i % 2 == 0)
-                {
-                    suma2 += (1 / (2 * (float)i + 1));
-                }
-                else
-                {
-                    suma2 -= (1 / (2 * (float)i + 1));
-                }
-            }
-            suma = suma + suma2 * 4;
-        }
-        static void calculoip4(Object numero)
-        {
-            float suma2 = 0;
-            int n = (int)numero;
-            for (int i = 30000; i < n; i++)
-            {
-                if (i % 2 == 0)
-                {
-                    suma2 += (1 / (2 * (float)i + 1));
-                }
-                else
-                {
-                    suma2 -= (1 / (2 * (float)i + 1));
-                }
-            }
-            suma = suma + suma2 * 4;
-        }
         static void Main(string[] args)
         {
-            Thread p1 = new Thread(calculoip)  { Name = "p1" };
-            Thread p2 = new Thread(calculoip2) { Name = "p2" };
-            Thread p3 = new Thread(calculoip3) { Name = "p3" };
-            Thread p4 = new Thread(calculoip4) { Name= "p4" };
-            p1.Start(10000);
-            p2.Start(20000);
-            p3.Start(30000);
-            p4.Start(40000);
+            Thread p1 = new Thread(calculopi)  { Name = "p1" };
+            Thread p2 = new Thread(calculopi) { Name = "p2" };
+            Thread p3 = new Thread(calculopi) { Name = "p3" };
+            Thread p4 = new Thread(calculopi) { Name= "p4" };
+            p1.Start(1);
+            p2.Start(2);
+            p3.Start(3);
+            p4.Start(4);
             Console.WriteLine("Calculo de numero pi con 4 hilos es: "+suma);
             Console.ReadKey();
         }
